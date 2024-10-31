5b7889669445360700e18a7b3be69f98f8f312d2
Dark Hole <dark.hole1@yandex.ru>
2024-10-31 23:13:28 +0300

    Improved flow

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,15 @@ type SessionData = {
         | 'start'
         | 'create-start-date'
         | 'create-select-date'
+        | 'create-deadline-date'
+        | 'create-chat'
         | 'create-rules'
         | 'create-restrictions'
         | 'create-additional-options'
+        | 'participate-info'
+        | 'participate-options'
+        | 'participate-select-title'
+        | 'participate-write-review'
 }
 
 type MyContext = Context & SessionFlavor<SessionData>
@@ -20,7 +26,10 @@ const bot = new Bot<MyContext>((config.Telegram as JsonMap).token as string)
 
 bot.use(session({ initial: (): SessionData => ({ state: 'start' }) }))
 
-bot.command('start', (ctx) => ctx.reply('Welcome'))
+bot.command('start', async (ctx) => {
+    await ctx.reply('Welcome')
+    ctx.session.state = 'participate-info'
+})
 
 bot.command('new', async (ctx) => {
     await ctx.reply('New santa')
@@ -32,6 +41,11 @@ const router = new Router<MyContext>((ctx) => ctx.session.state)
 router.route('create-start-date').on('message', async (ctx) => {
     await ctx.reply('Теперь выберите дату до которой нужно выбрать тайтл')
     ctx.session.state = 'create-select-date'
+})
+
+router.route('create-select-date').on('message', async (ctx) => {
+    await ctx.reply('Дедлайн')
+    ctx.session.state = 'create-deadline-date'
 })
 
 router.route('create-select-date').on('message', async (ctx) => {
@@ -53,6 +67,35 @@ router.route('create-additional-options').on('message', async (ctx) => {
     await ctx.reply(
         'Вы успешно создали новую Санту! Не забывайте одобрять участников'
     )
+    ctx.session.state = 'start'
+})
+
+router.route('participate-info').on('message', async (ctx) => {
+    await ctx.reply('Пара настроек')
+    ctx.session.state = 'participate-options'
+})
+
+router.route('participate-options').on('message', async (ctx) => {
+    await ctx.reply('Вы отправили заявку, дождитесь одобрения администратором')
+    ctx.session.state = 'start'
+})
+
+router.route('create-additional-options').on('message', async (ctx) => {
+    await ctx.reply(
+        'Вы успешно создали новую Санту! Не забывайте одобрять участников'
+    )
+    ctx.session.state = 'start'
+})
+
+router.route('participate-select-title').on('message', async (ctx) => {
+    await ctx.reply(
+        'Вы выбрали тайтл. Когда настанет час он отправится вашему подопечному :3'
+    )
+    ctx.session.state = 'start'
+})
+
+router.route('participate-write-review').on('message', async (ctx) => {
+    await ctx.reply('Вы написали ревью!')
     ctx.session.state = 'start'
 })
 
