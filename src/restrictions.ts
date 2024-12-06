12df1c7760f713283d06062566a26f144fcb0db3
Dark Hole <dark.hole1@yandex.ru>
2024-12-06 11:10:37 +0300

    Add parse restrictions

--- src/restrictions.ts
+++ src/restrictions.ts
@@ -1,5 +1,20 @@
-export type Restriction = {}
+export type Restriction = {
+    type: 'score' | 'episodes' | 'time'
+    operator: '>' | '<'
+    value: number
+}
 export const parseRestrictions = (text: string): Restriction[] | null => {
-    // TODO: Restrictions parsing
-    return []
+    const lines = text.split('\n')
+    const parsedLines = lines.map((line) => {
+        const match = line.match(/(score|episodes|time)(<|>)(\d+(?:\.\d+))/)
+        if (!match) {
+            return null
+        }
+        return {
+            type: match[1],
+            operator: match[2],
+            value: parseInt(match[3]),
+        } as Restriction
+    })
+    return parsedLines.filter((e) => e != null)
 }
