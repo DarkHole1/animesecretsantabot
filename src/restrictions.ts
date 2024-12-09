85b6d477aa0cb8186d0b9bfd98fc158db333bfef
DarkHole1 <dark.hole1@yandex.ru>
2024-12-09 12:08:30 +0300

    More logging

--- src/restrictions.ts
+++ src/restrictions.ts
@@ -9,17 +9,19 @@ export type Restriction = {
 export const parseRestrictions = (text: string): Restriction[] | null => {
     const lines = text.split('\n')
     const parsedLines = lines.map((line) => {
-        const match = line.match(/(score|episodes|duration)(<|>)(\d+(?:\.\d+)?)/)
+        const match = line.match(
+            /(score|episodes|duration)(<|>)(\d+(?:\.\d+)?)/
+        )
         if (!match) {
             return null
         }
         return {
             type: match[1],
             operator: match[2],
-            value: parseFloat(match[3]), 
+            value: parseFloat(match[3]),
         } as Restriction
     })
-    if(parsedLines.some(e => e == null)) {
+    if (parsedLines.some((e) => e == null)) {
         return null
     }
     return parsedLines.filter((e) => e != null)
@@ -43,10 +45,12 @@ export const checkShikimoriRestrictions = async (
     id: number,
     restrictions: Restriction[]
 ) => {
+    console.log(restrictions, id)
     const res = await axios.post(`https://shikimori.one/api/graphql`, {
         query: queryShikimori,
         variables: { ids: id.toString() },
     })
+    console.log(res.data)
     const title = res.data.data?.animes?.at(0)
     if (!title) {
         return false
@@ -65,6 +69,12 @@ export const checkShikimoriRestrictions = async (
                         restriction.value
                     )
                 ) {
+                    console.log(
+                        'score mismatch',
+                        restriction.operator,
+                        title.score,
+                        restriction.value
+                    )
                     return false
                 }
                 break
@@ -76,6 +86,12 @@ export const checkShikimoriRestrictions = async (
                         restriction.value
                     )
                 ) {
+                    console.log(
+                        'episodes mismatch',
+                        restriction.operator,
+                        title.episodes,
+                        restriction.value
+                    )
                     return false
                 }
                 break
@@ -87,6 +103,12 @@ export const checkShikimoriRestrictions = async (
                         restriction.value
                     )
                 ) {
+                    console.log(
+                        'duration mismatch',
+                        restriction.operator,
+                        title.duration,
+                        restriction.value
+                    )
                     return false
                 }
                 break
